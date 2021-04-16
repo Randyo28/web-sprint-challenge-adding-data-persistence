02fc976709b658ef2b1fdbb1c0bdab753c77e249
Randy Ortiz <Randyortiz28@gmail.com>
2021-04-16 14:03:49 -0400

    Completed projects and resource routes working on tasks route

--- api/resource/router.js
+++ api/resource/router.js
@@ -3,8 +3,30 @@ const Resources = require('./model')
 
 const router = express.Router()
 
+const checkResource = (req, res, next) => {
+  const { resource_name } = req.body
+
+  if (!resource_name) {
+    res.status(400).json({ message: `Resource name is Required` })
+  } else {
+    next()
+  }
+}
+
 router.get('/', (req, res, next) => {
   Resources.getAll()
+    .then((resource) => {
+      res.json(resource)
+    })
+    .catch((err) => {
+      next(err)
+    })
+})
+
+router.post('/', checkResource, (req, res, next) => {
+  const newResource = req.body
+
+  Resources.create(newResource)
     .then((resource) => {
       res.json(resource)
     })
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,38 @@
 const express = require('express')
+const Task = require('./model')
+
+const router = express.Router()
+
+const checkPost = (req, res, next) => {
+  const { task_description, project_id } = req.body
+
+  if (!task_description) {
+    res.json({ message: `Task description is required` })
+  } else if (!project_id) {
+    res.json({ message: `project_id is required` })
+  } else {
+    next()
+  }
+}
+
+router.get('/', (req, res, next) => {
+  Task.getTasks()
+    .then((task) => {
+      res.json(task)
+    })
+    .catch((err) => {
+      next(err)
+    })
+})
+
+router.post('/', checkPost, (req, res, next) => {
+  Task.create()
+    .then((task) => {
+      res.json(task)
+    })
+    .catch((err) => {
+      next(err)
+    })
+})
+
+module.exports = router

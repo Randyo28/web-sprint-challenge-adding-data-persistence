5a0e3078cfb23d3568f186d5e8e3007f12931ed6
Randy Ortiz <Randyortiz28@gmail.com>
2021-04-16 10:53:23 -0400

    Created tables not on to seeds

--- data/migrations/20210416143343_table.js
+++ data/migrations/20210416143343_table.js
@@ -13,13 +13,39 @@ exports.up = function (knex) {
     })
     .createTable('tasks', (tbl) => {
       tbl.increments('task_id')
+      tbl.text('task_description', 250).notNullable()
+      tbl.text('task_notes', 250)
+      tbl.boolean('task_completed')
+      tbl
+        .integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('project_id')
+        .inTable('projects')
+        .onDelete('Restrict')
     })
-    .createTable()
+    .createTable('project_resources', (tbl) => {
+      tbl.increments('project_resource_id')
+      tbl
+        .integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('project_id')
+        .inTable('projects')
+        .onDelete('Restrict')
+      tbl
+        .integer('resource_id')
+        .unsigned()
+        .notNullable()
+        .references('resource_id')
+        .inTable('resources')
+        .onDelete('Restrict')
+    })
 }
 
 exports.down = function (knex) {
   return knex.schema
-    .dropTableIfExists('')
+    .dropTableIfExists('project_resources')
     .dropTableIfExists('tasks')
     .dropTableIfExists('resources')
     .dropTableIfExists('projects')

ab7a4611a5d10b3ede4085e19a514f99cbe8d525
Randy Ortiz <Randyortiz28@gmail.com>
2021-04-16 09:52:00 -0400

    Initial commit

--- api/server.js
+++ api/server.js
@@ -1,1 +1,11 @@
-// build your server here and require it from index.js
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+server.use('*', (req, res) => {
+  res.send('Hey Grader!')
+})
+
+module.exports = server
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// start your server here
+const server = require('./api/server')
+
+const PORT = 5000
+
+server.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT} `)
+})
